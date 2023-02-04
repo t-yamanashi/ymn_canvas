bdee8b7b72e9836616fd2f0657fff154a9ccff78
t-yamanashi <ae86bg5@yahoo.co.jp>
2023-02-05 08:50:15 +0900

    Line検証

--- lib/ymn_canvas_web/live/canvas_live/index.ex
+++ lib/ymn_canvas_web/live/canvas_live/index.ex
@@ -4,37 +4,44 @@ defmodule YmnCanvasWeb.CanvasLive.Index do
 
   @impl true
   def mount(_params, _session, socket) do
-    Process.send_after(self(), :update, 100)
+    Process.send_after(self(), :update, 25)
 
     {:ok,
      socket
-     |> assign(data: create_data())}
+     |> assign(data: [])
+     |> assign(x: 1)
+    }
   end
 
   @impl true
   def handle_info(:update, socket) do
-    Process.send_after(self(), :update, 100)
-
+    Process.send_after(self(), :update, 25)
+    x = socket.assigns.x  +  1
+    x = rem(x, 600)
+    IO.inspect(x)
     {:noreply,
      socket
-     |> assign(data: create_data())}
+     |> assign(x: x)
+     |> assign(data: create_data(x))}
   end
 
-  def create_data() do
+  def create_data(x) do
     [
+      #fill_style("#FFFFFF"),
+      #fill_rect(0, 0, 1024, 768),
       #fill_style(Enum.random(["red","green"])),
       #/fill_rect(Enum.random(10..300), Enum.random(10..300), Enum.random(1..5), Enum.random(1..5))
-      fill_style("#FFFFBB"),
-      fill_rect(1, 1, 640, 480),
-      fill_style("#FFFFDD"),
-      fill_rect(10, 10, 620, 460),
-      stroke_style("red"),
-      begin_path(),
-      arc(300, 200, 100, 0, 2 * :math.pi()),
+      # fill_style("#FFFFBB"),
+      # fill_rect(1, 1, 640, 480),
+      # fill_style("#FFFFDD"),
+      # fill_rect(10, 10, 620, 460),
+      stroke_style("#0000FF"),
+      # begin_path(),
+      # arc(300, 200, 100, 0, 2 * :math.pi()),
+      # move_to(0, 0),
+      # line_to(100, 100),
       move_to(0, 0),
-      line_to(100, 100),
-      move_to(400, 400),
-      line_to(500, 300),
+      line_to(x * 8, 300),
       stroke()
     ]
   end
