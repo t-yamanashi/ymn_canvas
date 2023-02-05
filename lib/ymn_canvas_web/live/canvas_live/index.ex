46bb406d339d496e567c4961aef438968c00ceda
t-yamanashi <ae86bg5@yahoo.co.jp>
2023-02-05 09:54:29 +0900

    アニメーション検証

--- lib/ymn_canvas_web/live/canvas_live/index.ex
+++ lib/ymn_canvas_web/live/canvas_live/index.ex
@@ -15,9 +15,9 @@ defmodule YmnCanvasWeb.CanvasLive.Index do
 
   @impl true
   def handle_info(:update, socket) do
-    Process.send_after(self(), :update, 25)
+    Process.send_after(self(), :update, 12)
     x = socket.assigns.x  +  1
-    x = rem(x, 600)
+    x = rem(x, 900)
     IO.inspect(x)
     {:noreply,
      socket
@@ -26,22 +26,37 @@ defmodule YmnCanvasWeb.CanvasLive.Index do
   end
 
   def create_data(x) do
+    r = :math.pi() / 180
+    yy = :math.cos(x * r) * (x / 2)
+    yy = yy + 384
+
+    xx = :math.sin(x * 1.2 * r) * (x / 2)
+    xx = xx + 512
+
+
+    |> IO.inspect()
     [
-      #fill_style("#FFFFFF"),
-      #fill_rect(0, 0, 1024, 768),
+      fill_style("#000000"),
+      fill_rect(0, 0, 1024, 768),
       #fill_style(Enum.random(["red","green"])),
       #/fill_rect(Enum.random(10..300), Enum.random(10..300), Enum.random(1..5), Enum.random(1..5))
       # fill_style("#FFFFBB"),
       # fill_rect(1, 1, 640, 480),
       # fill_style("#FFFFDD"),
       # fill_rect(10, 10, 620, 460),
-      stroke_style("#0000FF"),
+      stroke_style("#00FF00"),
       # begin_path(),
-      # arc(300, 200, 100, 0, 2 * :math.pi()),
-      # move_to(0, 0),
-      # line_to(100, 100),
+      arc(xx, yy, 20, 0, 2 * :math.pi()),
+      move_to(512, 384),
+      line_to(xx, yy),
       move_to(0, 0),
-      line_to(x * 8, 300),
+      line_to(xx, yy),
+      move_to(1024, 768),
+      line_to(xx, yy),
+      move_to(0, 768),
+      line_to(xx, yy),
+      move_to(1024, 0),
+      line_to(xx, yy),
       stroke()
     ]
   end
