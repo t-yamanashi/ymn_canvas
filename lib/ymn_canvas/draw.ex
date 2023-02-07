edb8a1246d29f446edc303fd22c123a6fe6a9d02
t-yamanashi <ae86bg5@yahoo.co.jp>
2023-02-08 08:20:11 +0900

    Atom化

--- lib/ymn_canvas/draw.ex
+++ lib/ymn_canvas/draw.ex
@@ -7,11 +7,11 @@ defmodule YmnCanvas.Draw do
   @doc """
   塗りつぶす色を指定する
   """
-  def fill_style(style), do: ["fillStyle", style]
-  def stroke_style(style), do: ["strokeStyle", style]
-  def begin_path(), do: ["beginPath"]
-  def stroke(), do: ["stroke"]
-  def arc(x, y, radius, start_angle, end_angle), do: ["arc", x, y, radius, start_angle, end_angle]
-  def move_to(x, y), do: ["moveTo", x, y]
-  def line_to(x, y), do: ["lineTo", x, y]
+  def fill_style(style), do: [:fillStyle, style]
+  def stroke_style(style), do: [:strokeStyle, style]
+  def begin_path(), do: [:beginPath]
+  def stroke(), do: [:stroke]
+  def arc(x, y, radius, start_angle, end_angle), do: [:arc, x, y, radius, start_angle, end_angle]
+  def move_to(x, y), do: [:moveTo, x, y]
+  def line_to(x, y), do: [:lineTo, x, y]
 end
