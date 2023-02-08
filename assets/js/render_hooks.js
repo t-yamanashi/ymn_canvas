2f524c56b184a3f5cf79169c02917e40c7fd909a
t-yamanashi <ae86bg5@yahoo.co.jp>
2023-02-08 09:19:44 +0900

    描画関数のリファクタリング

--- assets/js/render_hooks.js
+++ assets/js/render_hooks.js
@@ -1,54 +1,41 @@
 export const hooks = {
-    canvas: {
-        mounted() {
-            console.log("mount_hooks");
-            render(JSON.parse(this.el.dataset.canvas));
-        },
-        updated() {
-            console.log("update_hooks");
-            render(JSON.parse(this.el.dataset.canvas));
-        }
-    }
-}
+  canvas: {
+    mounted() {
+      render(JSON.parse(this.el.dataset.canvas));
+    },
+    updated() {
+      render(JSON.parse(this.el.dataset.canvas));
+    },
+  },
+};
 
 const render = (datas) => {
-    const canvasBuffer = document.querySelector('#canvas_buffer');
-    const ctxBuffer = canvasBuffer.getContext('2d');
-    datas.map(data => render_exec(ctxBuffer, data));    
-    const canvasArea = document.querySelector('#canvas_area');
-    const ctxArea = canvasArea.getContext('2d');
-    bufferImage = ctxBuffer.getImageData(0, 0, 1024, 768);
-    ctxArea.putImageData(bufferImage, 0, 0);
-  }
+  const canvasBuffer = document.querySelector("#canvas_buffer");
+  const ctxBuffer = canvasBuffer.getContext("2d");
+  datas.map((data) => render_exec(ctxBuffer, data));
+  const canvasArea = document.querySelector("#canvas_area");
+  const ctxArea = canvasArea.getContext("2d");
+  bufferImage = ctxBuffer.getImageData(0, 0, 1024, 768);
+  ctxArea.putImageData(bufferImage, 0, 0);
+};
 
 const render_exec = (ctx, data) => {
-    [fn, ...arg] = data
-    switch (fn) {
-        case "fillRect":
-            ctx.fillRect(...arg);
-            break;
-        case 'fillStyle':
-            ctx.fillStyle = arg;
-            break;
-        case 'strokeStyle':
-            ctx.strokeStyle = arg;
-            break;
-        case 'beginPath':
-            ctx.beginPath();
-            break;
-        case 'stroke':
-            ctx.stroke();
-            break;
-        case 'arc':
-            ctx.arc(...arg);
-            break;
-        case "moveTo":
-            ctx.moveTo(...arg);
-            break;
-        case "lineTo":
-            ctx.lineTo(...arg);
-            break;
-        default:
-            console.log("no function");
-    }
-}
+  [fn_name, ...arg] = data;
+  fn = render_function[fn_name];
+  if (fn == undefined) {
+    console.log("no function");
+    return;
+  }
+  fn(ctx, arg);
+};
+
+const render_function = {
+  fillRect: (ctx, arg) => ctx.fillRect(...arg),
+  fillStyle: (ctx, arg) => (ctx.fillStyle = arg),
+  strokeStyle: (ctx, arg) => (ctx.strokeStyle = arg),
+  beginPath: (ctx, _arg) => ctx.beginPath(),
+  stroke: (ctx, _arg) => ctx.stroke(),
+  arc: (ctx, arg) => ctx.arc(...arg),
+  moveTo: (ctx, arg) => ctx.moveTo(...arg),
+  lineTo: (ctx, arg) => ctx.lineTo(...arg),
+};
